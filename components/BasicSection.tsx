0c2c9cbc47d3444f1d6457cbcc540cab0ef17a7f
Mochammad Ilham Adrian <muhamadilhamadrian0@gmail.com>
2025-05-29 21:58:06 +0700

    change style image

--- components/BasicSection.tsx
+++ components/BasicSection.tsx
@@ -17,7 +17,7 @@ export default function BasicSection({ imageUrl, title, overTitle, reversed, chi
   return (
     <BasicSectionWrapper reversed={reversed}>
       <ImageContainer>
-        <img src={imageUrl} alt={title} style={{ objectFit: 'cover', width: '100%', height: '100%', position: 'absolute' }} />
+        <img src={imageUrl} alt={title} style={{ objectFit: 'cover' }} />
       </ImageContainer>
       <ContentContainer>
         <CustomOverTitle>{overTitle}</CustomOverTitle>
