dc405b009fb5656bc1ee9ae2602def93bb9369a2
Mochammad Ilham Adrian <muhamadilhamadrian0@gmail.com>
2025-05-29 22:17:58 +0700

    about update

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,6 +5,7 @@ import Page from 'components/Page';
 import SectionTitle from 'components/SectionTitle';
 import YoutubeVideo from 'components/YoutubeVideo';
 import { media } from 'utils/media';
+import FaqSection from 'views/PricingPage/FaqSection';
 
 const FEATURES = [
   {
@@ -65,15 +66,16 @@ const FEATURES = [
 
 export default function FeaturesPage() {
   return (
-    <Page title="Features" description="Elit aute do nisi Lorem id ea culpa sint duis eu tempor dolore elit.">
+    <Page title="Tentang" description="Totebag Bandung 93 adalah brand lokal yang mengusung konsep tote bag berkualitas tinggi dengan desain kekinian dan fungsional. Kami berdiri dengan misi menyediakan tas yang stylish, ramah lingkungan, dan tahan lama untuk mendukung gaya hidup aktif dan sadar lingkungan masyarakat modern. Setiap produk Totebag Bandung 93 dibuat dari bahan pilihan seperti kanvas dan katun yang kuat serta ramah lingkungan. Kami percaya bahwa fashion tidak hanya soal penampilan, tapi juga keberlanjutan. Oleh karena itu, kami mengedepankan proses produksi yang bertanggung jawab dan ramah alam. Berlokasi di kota Bandung, kami menggabungkan sentuhan seni lokal dengan tren desain global untuk menghadirkan tote bag yang unik dan personal. Totebag Bandung 93 cocok untuk kamu yang ingin tampil gaya sambil berkontribusi pada pelestarian lingkungan.">
       <Wrapper>
         <SectionTitle>Check out this quick introduction</SectionTitle>
-        <YoutubeVideo url="https://www.youtube.com/watch?v=BggrpKfqh1c" />
+        {/* <YoutubeVideo url="https://www.youtube.com/watch?v=BggrpKfqh1c" />
         <CustomAutofitGrid>
           {FEATURES.map((singleFeature, idx) => (
             <BasicCard key={singleFeature.title} {...singleFeature} />
           ))}
-        </CustomAutofitGrid>
+        </CustomAutofitGrid> */}
+        <FaqSection />
       </Wrapper>
     </Page>
   );
