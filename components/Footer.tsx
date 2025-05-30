08b17c8eb41809981521f821a7accba7b29c741a
Mochammad Ilham Adrian <muhamadilhamadrian0@gmail.com>
2025-05-30 13:02:50 +0700

    add footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -57,25 +57,25 @@ export default function Footer() {
         </ListContainer>
         <BottomBar>
           <ShareBar>
-            <NextLink href="https://www.twitter.com/my-saas-startup" passHref>
+            <NextLink href="https://www.twitter.com/" passHref>
               <a>
                 <TwitterIcon size={50} round={true} />
               </a>
             </NextLink>
 
-            <NextLink href="https://www.facebook.com/my-saas-startup" passHref>
+            <NextLink href="https://www.facebook.com/" passHref>
               <a>
                 <FacebookIcon size={50} round={true} />
               </a>
             </NextLink>
 
-            <NextLink href="https://www.linkedin.com/my-saas-startup" passHref>
+            <NextLink href="https://www.linkedin.com/" passHref>
               <a>
                 <LinkedinIcon size={50} round={true} />
               </a>
             </NextLink>
           </ShareBar>
-          <Copyright>&copy; Copyright 2021 My Saas Startup</Copyright>
+          <Copyright>&copy; Copyright 2025 PT CREASIK CIPTAKARYA INDONESIA</Copyright>
         </BottomBar>
       </Container>
     </FooterWrapper>
