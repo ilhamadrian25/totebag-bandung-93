7e7e736a5ba700579d40743a0e7dff9ddf298976
Mochammad Ilham Adrian <muhamadilhamadrian0@gmail.com>
2025-05-30 13:01:50 +0700

    add catalog

--- pages/catalog.tsx
+++ pages/catalog.tsx
@@ -2,13 +2,25 @@ import styled from 'styled-components';
 import Page from 'components/Page';
 import FaqSection from 'views/PricingPage/FaqSection';
 import PricingTablesSection from 'views/PricingPage/PricingTablesSection';
+import ScrollableCatalog from 'views/HomePage/ScrollableCatalog';
+import { getAllPosts } from 'utils/postsFetcher';
+import { InferGetStaticPropsType } from 'next';
 
-export default function PricingPage() {
+export async function getStaticProps() {
+  return {
+    props: {
+      posts: await getAllPosts(),
+    },
+  };
+}
+
+
+export default function PricingPage({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <Page title="Pricing" description="Cupidatat et reprehenderit ullamco aute ullamco anim tempor.">
+    <Page title="Katalog" description="Katalog produk kami.">
       <Wrapper>
         {/* <PricingTablesSection /> */}
-        <FaqSection />
+        <ScrollableCatalog posts={[]} />
       </Wrapper>
     </Page>
   );
