8998445b027e035d6e02833486704b89b15fed40
Mochammad Ilham Adrian <muhamadilhamadrian0@gmail.com>
2025-05-30 14:03:58 +0700

    update

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const LogoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const LogoImage = styled.img`
+  margin-right: 0.5rem;
+  width: 40px;   /* Bisa sesuaikan ukuran logo */
+  height: auto;
+`;
+
+const LogoText = styled.span`
+  font-weight: bold;
+  font-size: 1.2rem;
+  color: #333;  /* Bisa disesuaikan */
+`;
 
 export default function Logo({ ...rest }) {
   return (
-    <div className="flex items-center" {...rest}>
-      <img src="/logo.png" alt="Logo Totebag Bandung 93" className="mr-2" />
-      <span>Totebag Bandung 93</span>
-    </div>
+    <LogoWrapper {...rest}>
+      <LogoImage src="/logo.png" alt="Logo Totebag Bandung 93" />
+      <LogoText>Totebag Bandung 93</LogoText>
+    </LogoWrapper>
   );
 }
